fbbe7df7160ac0f659a90b28732d3944f2def3dd
Joakim Nilsson <jocke@student251-209.eduroam.uu.se>
2016-05-04 13:20:48 +0200

    Bluetooth data will now store in json file as well

--- mqtt_test.py
+++ mqtt_test.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
 
 import paho.mqtt.client as paho
+import json
+
+data = {
+
+}
 
 client = paho.Client()
 
 def on_message(mosq, obj, msg):
-	s = msg.payload
-	#This is the location of the output file on my computer, you will have to update to yours.
-	fo = open("/Users/jocke/Desktop/output.txt", "a")
-	fo.write( "%s" % (s) + "\n")
-	fo.close()
+	s = str(msg.payload)
+	if s.startswith("Temperature: "):
+		data["Temperature"] = s[13:-2]
+
+	if s.startswith("Vis: "):
+		data["Vis"] = s[5:-2]
+
+	if s.startswith("IR: "):
+		data["IR"] = s[4:-2]
+
+	if s.startswith("UV: "):
+		data["UV"] = s[4:-2]
+
+
+	with open('data.json', 'w') as f:
+		json.dump(data, f)
 
 def on_publish(mosq, obj, mid):
     pass
@@ -19,7 +35,7 @@ client.on_publish = on_publish
 
 client.connect("localhost", 1883)
 
-client.subscribe("test3maj", 0)
+client.subscribe("myfirst/test", 0)
 
 while client.loop() == 0:
     pass
